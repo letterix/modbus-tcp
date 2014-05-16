cd15a1d851179f3358d41858e0547f2bf85509df
Rasmus <Rasmus>
2014-05-16 16:03:50 +0200

    fixed small errors in the example and remove the pycache

--- read_example_own.py
+++ read_example_own.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from modbus.modbus_tcp import ModbusTcp
-from modbus import utils
 from modbus.exceptions import *
-logger = utils.create_logger("console")
 
 if __name__ == "__main__":
     try:
-        logger.info("test started")
         client = ModbusTcp(host = '192.168.0.1')
-        logger.info(client.read_holding_registers(5, 1))
+        print(client.read_holding_registers(5, 1))
 
     except ModbusError as e:
-        logger.error("%s- Code=%d" % (e, e.get_exception_code()))
+        print("%s- Code=%d" % (e, e.get_exception_code()))
